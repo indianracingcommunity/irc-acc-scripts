a3d33940e09193284475f3b2ee8aec317142be2f
kapilace6 <kapilace6@gmail.com>
2021-11-24 23:57:18 +0530

    Entry List API Placeholder

--- entryListCreator.py
+++ entryListCreator.py
@@ -1,6 +1,9 @@
 import json
 
 f = open('signuplatest.json', 'r')
+# API Endpoint - https://indianracingcommunity.co.in/api/signups/{season_id}
+# Requires Authorization Header
+# Returns [{id, drivername, discord_id, racenumber, steam_id, attendance, car}, {...} ...]
 
 data = json.load(f)
 
@@ -12,10 +15,12 @@ i = 0
 racenumbers = []
 shortnames = []
 
+# TODO: Check if id is null, if so skip and print Username<@discord_id> back to Discord -> Missing from website
+# TODO: Check if driver is in classes.json, else print Username<@discord_id> back to Discord -> Missing a classes
 for driver in data:
-    drivername = driver['UserName']
-    shortname = driver['UserName'][:3].upper()
-    racenumber = driver['drivernumber']
+    drivername = driver['drivername']
+    shortname = driver['drivername'][:3].upper()
+    racenumber = driver['racenumber']
 
     if racenumber in racenumbers:
         print(drivername + ' has a clash of race number ' + racenumber)
@@ -32,12 +37,12 @@ for driver in data:
                 'firstName': "",
                 'lastName': drivername,
                 'shortName': shortname,
-                'playerID': 'S' + driver['Steam'],
+                'playerID': 'S' + driver['steam_id'],
                 'driverCategory': 0
             }
             ],
             'raceNumber': int(racenumber),
-            "forcedCarModel": int(driver['Car Value 1']),
+            "forcedCarModel": int(driver['car']),
             "defaultGridPosition": -1,
             "overrideDriverInfo": 1,
             "isServerAdmin": 0
